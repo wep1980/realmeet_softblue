903efe27fef9921af3d9b0b0711733f2d2536b61
wep1980 <wepbike@gmail.com>
2022-12-21 08:24:48 -0300

    Initial commit

---
+++ service/src/main/java/br/com/wepdev/realmeet_softblue/RoomController.java
@@ -0,0 +1,19 @@
+package br.com.wepdev.realmeet_softblue;
+
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
+import br.com.wepdev.realmeet_softblue.api.facade.RoomsApi;
+import br.com.wepdev.realmeet_softblue.api.model.Room;
+import java.util.concurrent.CompletableFuture;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RoomController implements RoomsApi {
+
+    @Override
+    public CompletableFuture<ResponseEntity<Room>> listRooms(Long id) {
+
+        return CompletableFuture.supplyAsync(() -> ResponseEntity.ok(new Room().id(1L).name("Room 1")));
+    }
+}
